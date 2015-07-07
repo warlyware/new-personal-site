34666d1ce39c56967b761458bc763fb058279b32
warlyware <danward@gmail.com>
2015-07-07 14:32:15 -0700

    First commit

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+    $('.name').lettering();
+    $('.tagline').lettering();
+    $('.tagline-two').lettering();
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
