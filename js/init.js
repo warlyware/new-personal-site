1e04f47c7f11be52516808f556f22076f775ab04
warlyware <danward@gmail.com>
2015-07-08 01:17:41 -0700

    Add styling, proficiencies, social media

--- js/init.js
+++ js/init.js
@@ -1,8 +1,8 @@
 (function($){
   $(function(){
     $('.name').lettering();
-    $('.tagline').lettering();
-    $('.tagline-two').lettering();
+    // $('.tagline').lettering();
+    // $('.tagline-two').lettering();
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
