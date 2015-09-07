9ad2e7d2cc07d6e7351479a77dcf7ebda572d8ba
warlyware <danward@gmail.com>
2015-09-07 11:27:49 -0400

    Update images for projects

--- js/init.js
+++ js/init.js
@@ -6,6 +6,5 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.scrollspy').scrollSpy();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  });
+})(jQuery);
